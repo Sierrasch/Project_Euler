6e0f3894685a9da07d760d0d254c39fa5e57b531
Sierra Schwellenbach <sschwellenbach@umail.ucsb.edu>
2016-10-18 13:18:09 +0100

    updated isPrime function

--- Useful_Code/Funcs.cpp
+++ Useful_Code/Funcs.cpp
@@ -1,9 +1,12 @@
+#include <math.h>
 
 //isPrime method using unsigned long long
 bool isPrime (unsigned long long input){
   if (input == 0 || input == 1)
     return false;
-  for (unsigned long long i = 2; i < input/2 + 1; i++){
+  if (input == 2)
+    return true;
+  for (unsigned long long i = 2; i < sqrt(input) + 1; i++){
     if (input % i == 0)
       return false;
   }
@@ -14,7 +17,9 @@ bool isPrime (unsigned long long input){
 bool isPrime (int input){
   if (input == 0 || input == 1)
     return false;  
-  for (int i = 2; i < input/2 + 1; i++){
+  if (input == 2)
+    return true;
+  for (int i = 2; i < sqrt(input) + 1; i++){
     if (input % i == 0)
       return false;
   }
