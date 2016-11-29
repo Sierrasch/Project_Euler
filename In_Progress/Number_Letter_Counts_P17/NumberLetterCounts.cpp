30ed4906924d04562ffce81d01f7edbd1b6b3184
Sierra Schwellenbach <sschwellenbach@umail.ucsb.edu>
2016-11-29 12:27:14 +0000

    restructured into completed and in progress folders

---
+++ In_Progress/Number_Letter_Counts_P17/NumberLetterCounts.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int onesPlaceVals[] = { 4/*zero*/, 3/*one*/, 3/*two*/, 5, 4, 4, 3, 5, 5, 4};
+  int tensPlaceVals[] = { 0, 0/*teens NOT USED*/, 6/*twenties*/, 6, 6, 5, 5, 7, 6, 6 };
+  int teensVals[]     = { 3 /*ten*/, 6/*eleven*/, 6, 8, 8, 7, 7, 9, 8, 8 };
+  return 0;
+}
